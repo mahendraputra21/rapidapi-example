cad5ff9670337d11fb6b36dc465114301acf23e4
Dewa Mahendra <31196162+mahendraputra21@users.noreply.github.com>
2021-10-24 10:05:36 +0800

    Create users.js

---
+++ data/users.js
@@ -0,0 +1,29 @@
+const users = 
+[
+  {
+    "email": "dewa.mahendra@mailinator.com",
+    "phone_number": "082221187",
+    "location": {
+      "street": "jl. subak dalem perumahan",
+      "city": "Denpasar",
+      "state": "Indonesia",
+      "postcode": 55822
+    },
+    "first_name": "Dewa",
+    "last_name": "Mahendra"
+  },
+  {
+    "email": "wahyubos@ninja.com",
+    "phone_number": "08226678966",
+    "location": {
+      "street": "3391 Peguyangan",
+      "city": "Denpasar",
+      "state": "Indonesia",
+      "postcode": 88520
+    },
+    "first_name": "Putu",
+    "last_name": "Wahyu"
+  }
+]
+
+module.exports = users;
